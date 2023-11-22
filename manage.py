9469c9a9b3c6fc82478a72b2fab6f2eb92a648f8
Baldens <den23.2001@gmail.com>
2023-11-22 21:53:28 +0500

    Push object names search (NER)

--- manage.py
+++ manage.py
@@ -52,3 +52,16 @@ def summarize(text, ratio=50):
     indices = list(sorted([idx for _, idx in pairs]))
     summary = " ".join([sentences[idx] for idx in indices])
     return summary
+
+
+nlp = ru_core_news_lg.load()
+def searchNER(text):
+    doc = nlp(text)
+    colors = {"PER": "#9966cc", "ORG": "green", "LOC": "#1dacd6", "Date": "red", "Money": "yellow"}
+    edited_text = ""
+    for token in doc:
+        if token.ent_type_ in colors.keys():
+            edited_text += "<span style=\"color:" + colors[token.ent_type_] + ";\">"+ token.text +"</span> "
+        else:
+            edited_text += token.text + ' '
+    return edited_text
