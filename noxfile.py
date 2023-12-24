8a4ad4ec6016968de894e725cc8b2d6a5c6f4231
Denis M <146358294+DennisSupport@users.noreply.github.com>
2023-12-24 16:54:27 +0500

    Create noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+import nox
+
+@nox.session(python=["3.9", "3.10", "3.11"])
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", ".")
+
+@nox.session(python=["3.9", "3.10", "3.11"])
+def tests(session):
+    session.install("-r", "requirements.txt")
+    session.run("pytest")
